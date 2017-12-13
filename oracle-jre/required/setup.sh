24bdd6b0147a04c9f0f9d6fb0546353f51e8aacf
Mykhailo Yashchuk <19mihail@gmail.com>
2017-12-13 17:28:44 +0200

    Updates configuration script for oracle-jre

--- oracle-jre/required/setup.sh
+++ oracle-jre/required/setup.sh
@@ -3,10 +3,10 @@
 set -uex
 
 echo "Updating packages ..."
-yum -yq update
+yum -y -q update
 
 echo "Installing additional packages ..."
-yum -yq install wget zip unzip
+yum -y -q install wget zip unzip
 
 echo "Downloading JRE ..."
 wget -nv --no-cookies --no-check-certificate \
@@ -16,8 +16,7 @@ wget -nv --no-cookies --no-check-certificate \
 echo "Installing JRE ..."
 mkdir -p "${JAVA_HOME}"
 tar -xzvf "${JRE_ARTIFACT_PATH}" -C "${JAVA_HOME}" --strip-components=1
-ln -sf /usr/bin/java ${JAVA_HOME}/bin/java
-ln -sf /usr/bin/jar ${JAVA_HOME}/bin/jar
+alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
 
 # Clean
 rm -f "${JRE_ARTIFACT_PATH}"
