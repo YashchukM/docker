b22934e07ef976fa249f2e086024a3b112887c2c
Mykhailo Yashchuk <19mihail@gmail.com>
2017-12-14 16:01:23 +0200

    Finishes ZK subproject, modifies oracle-jre

--- oracle-jre/required/setup.sh
+++ oracle-jre/required/setup.sh
@@ -2,27 +2,25 @@
 
 set -uex
 
-echo "Updating packages ..."
+# Updating packages
 yum -y -q update
 
-echo "Installing additional packages ..."
+# Installing additional packages
 yum -y -q install wget zip unzip
 
-echo "Downloading JRE ..."
+# Downloading JRE
 wget -nv --no-cookies --no-check-certificate \
   --header "Cookie: oraclelicense=accept-securebackup-cookie" \
   "${JRE_URL}" -O ${JRE_ARTIFACT_PATH}
 
-echo "Installing JRE ..."
+# Installing JRE
 mkdir -p "${JAVA_HOME}"
 tar -xzvf "${JRE_ARTIFACT_PATH}" -C "${JAVA_HOME}" --strip-components=1
-alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
+alternatives --install "/usr/bin/java" java "${JAVA_HOME}/bin/java" 1
 
 # Clean
 rm -f "${JRE_ARTIFACT_PATH}"
-rm -rf /tmp/*
+rm -rf "/tmp/*"
 
 yum remove -y wget zip unzip
 yum clean all
-
-echo "JRE setup done."
--- zookeeper/required/setup.sh
+++ zookeeper/required/setup.sh
@@ -6,11 +6,31 @@ set -uex
 yum -y -q install wget zip unzip
 
 # Download archive
+mkdir -p "/opt"
 wget -nv "${ZK_URL}" -O "${ZK_ARTIFACT_PATH}"
 
 # Install ZK
-mkdir -p "${ZK_HOME}"
-tar -xzvf "${ZK_ARTIFACT_PATH}" -C /opt --strip-components=1
-alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
+mkdir -p "/opt/zookeeper-${ZK_VERSION}"
+tar -xzf "${ZK_ARTIFACT_PATH}" -C "/opt/zookeeper-${ZK_VERSION}" --strip-components=1
+ln -sf "/opt/zookeeper-${ZK_VERSION}" "${ZK_HOME}"
+
+# Configure ZK
+rm -rf "${ZK_HOME}/conf/*"
+chmod +x "/tmp/entrypoint.sh"
+mv "/tmp/conf/*" "${ZK_HOME}/conf"
+mv "/tmp/entrypoint.sh" "${ZK_HOME}"
+
+# Configure ownership
+mkdir -p "/data/zookeeper"
+mkdir -p "/logs/zookeeper"
+useradd -U zookeeper -d "${ZK_HOME}"
+chown -RL zookeeper:zookeeper "${ZK_HOME}"
+chown -R zookeeper:zookeeper "/data/zookeeper"
+chown -R zookeeper:zookeeper "/logs/zookeeper"
 
 # Clean
+rm -f "${ZK_ARTIFACT_PATH}"
+rm -rf "/tmp/*"
+
+yum remove -y wget zip unzip
+yum clean all
